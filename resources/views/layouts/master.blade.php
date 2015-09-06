2435f8eaaa567849a42a33c1faef37f29c0e14db
4unkur <4unkur@mail.ru>
2015-09-06 22:14:15 +0600

    meta for responsive added, minor layout changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,10 +1,10 @@
+<div class="container">
 @include('partials.header')
-<div class="content col-md-6">
 @yield('content')
     @if(Session::has('message'))
         <div class="alert alert-info">
             {{ Session::get('message') }}
         </div>
     @endif
-</div>
 @include('partials.footer')
+</div>
